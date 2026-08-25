ebfe9f0c0cd2fe9ae145111aa38d55d8e108750b
sanshre-sandip <sandpipe98@gmail.com>
2026-08-25 11:45:55 +0545

    Update package namespace and application ID to match new structure; add MainActivity for ride application

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.gogo"
+    namespace = "com.gogo.rides"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gogo"
+        
+        applicationId = "com.gogo.rides"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/gogo/rides/MainActivity.kt
+++ android/app/src/main/kotlin/com/gogo/rides/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.gogo
+package com.gogo.rides
 
 import io.flutter.embedding.android.FlutterActivity
 
